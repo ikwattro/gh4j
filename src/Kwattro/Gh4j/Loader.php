97a822989f47b94fce57d3575517283f40cfe549
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-08-14 14:13:34 +0200

    new instance is created

--- src/Kwattro/Gh4j/Loader.php
+++ src/Kwattro/Gh4j/Loader.php
@@ -50,6 +50,7 @@ class Loader
 				return $this->loadedEventLoaders[$eventType];
 				break;
 			case 'ForkEvent':
+			return new ForkEventLoader();
 				if (!array_key_exists($eventType, $this->loadedEventLoaders)) {
 					$loader = new ForkEventLoader();
 					$this->loadedEventLoaders[$eventType] = $loader;
